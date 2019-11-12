99d7688e5cde549c24b4e0ab66d9a3dbb4d11746
Sara Fotros <sarafotros1@yahoo.com>
2019-11-12 17:45:31 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,77 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+ 	fifa_2020 = {
+	  :team_a => {},
+	  :team_b => {},
+	  :team_c => {}
+	}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  fifa_2020{
+	  :team_a => {
+	    :euro => {},
+	    :asia => {},
+	    :america => {},
+	    :africa => {}
+	  },
+	  :team_b => {
+	    :euro => {},
+	    :asia => {},
+	    :america => {},
+	    :africa => {}
+	  },
+	  :team_c => {
+	    :euro => {},
+	    :asia => {},
+	    :america => {},
+	    :africa => {}
+	  }
+	}
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ fifa_2020 = {
+	  :team_a => {
+	    :euro => {:france => {} },
+	    :asia => {:korea => {} },
+	    :america => {:usa => {} },
+	    :africa => {:egypt => {} }
+	  },
+	  :team_b => {
+	    :euro => {:uk => {}},
+	    :asia => {:iran =>  {}},
+	    :america => {:brazil => {}},
+	    :africa => { :jamaica => {}}
+	  },
+	  :team_c => {
+	    :euro => { :germany => {}},
+	    :asia => { :japan => {}},
+	    :america => { :mexic => {}},
+	    :africa => {:somaly => {}}
+	  }
+	}
+end
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ fifa_2020 = {
+	  :team_a => {
+	    :euro => {:france => {coach = "alan", age = "54"} },
+	    :asia => {:korea => {coach = "alan", age = "54"} },
+	    :america => {:usa => {coach = "alan", age = "54"} },
+	    :africa => {:egypt => {coach = "alan", age = "54"} }
+	  },
+	  :team_b => {
+	    :euro => {:uk => {coach = "alan", age = "54"}},
+	    :asia => {:iran =>  {coach = "alan", age = "54"}},
+	    :america => {:brazil => {coach = "alan", age = "54"}},
+	    :africa => { :jamaica => {coach = "alan", age = "54"}}
+	  },
+	  :team_c => {
+	    :euro => { :germany => {coach = "alan", age = "54"}},
+	    :asia => { :japan => {coach = "alan", age = "54"}},
+	    :america => { :mexic => {coach = "alan", age = "54"}},
+	    :africa => {:somaly => {coach = "alan", age = "54"}}
+	  }
+	}
 end
