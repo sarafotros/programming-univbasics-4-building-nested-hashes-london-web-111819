ecf74fba7bbe0c6540adb99c4b847de3b575d64e
Sara Fotros <sarafotros1@yahoo.com>
2019-11-12 17:43:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,8 +54,24 @@ end
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ fifa_2020 = {
+	  :team_a => {
+	    :euro => {:france => {} },
+	    :asia => {:korea => {} },
+	    :america => {:usa => {} },
+	    :africa => {:egypt => {} }
+	  },
+	  :team_b => {
+	    :euro => {:uk => {}},
+	    :asia => {:iran =>  {}},
+	    :america => {:brazil => {}},
+	    :africa => { :jamaica => {}}
+	  },
+	  :team_c => {
+	    :euro => { :germany => {}},
+	    :asia => { :japan => {}},
+	    :america => { :mexic => {}},
+	    :africa => {:somaly => {}}
+	  }
+	}
 end
